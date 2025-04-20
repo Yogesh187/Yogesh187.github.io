998a796df88439dcd18cca6efd448e5397b77afc
MadheshN <madheshnarayanamurthi2002@gmail.com>
2025-04-20 22:10:10 +0530

    added drop down

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import "bootstrap/dist/js/bootstrap.bundle.min.js";
 import "bootstrap/dist/js/bootstrap.min.js";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min";
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 
 
 import Login from "./Pages/Login";
